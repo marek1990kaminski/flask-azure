e6fb15b29a75955ad7d876de6e469c144dfbe287
jon <marek1990kaminski@gmail.com>
2022-02-28 18:01:55 +0100

    postgre db initially set

---
+++ README.md
@@ -0,0 +1,9 @@
+## commands to build flask app:
+
+first enter container:  
+`docker-compose exec backend /bin/bash`
+
+inside run:  
+`python manager.py db init` - this will cause migrations folder to appear  
+`python manager.py db migrate` - this will trigger 'alembic_version' table to appear  
+`python manager.py db upgrade` - this will trigger actual migration
--- main.py
+++ main.py
@@ -1,7 +1,43 @@
+from dataclasses import dataclass
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://root:root@micro-main-db/main'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://root:root@azure-postgres-db/main'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
 db: SQLAlchemy = SQLAlchemy(app)
+
+
+@dataclass
+class ToDoTask(db.Model):
+    id: int
+    user_id: str
+    text: str
+    deleted: bool
+    done: bool
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.String(200))
+    text = db.Column(db.String(200))
+    deleted = db.Column(db.Boolean())
+    done = db.Column(db.Boolean())
+
+
+@dataclass
+class User(db.Model):
+    id: int
+    text: str
+    deleted: bool
+    done: bool
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.String(200))
+    text = db.Column(db.String(200))
+    deleted = db.Column(db.Boolean())
+    done = db.Column(db.Boolean())
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
